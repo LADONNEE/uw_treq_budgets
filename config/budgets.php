7cff7d7b28e68049493082b56888ae11f0896325
Nadhir <nadhir@ladonnee.com>
2022-01-14 10:48:29 +0100

    Budgets scope 2

--- config/budgets.php
+++ config/budgets.php
@@ -9,8 +9,11 @@ return [
             '263' => 'COLLEGE OF ENVIRNMNT',
         ],
         'org-codes' => [
-            '2552158000' => 'AP DEG - EDUCATION',
-        ],
+/*            '2552158000' => 'AP DEG - EDUCATION',
+            '2552258000' => 'AP CR CERT-EDUCATION',
+            '2552558000' => 'AP NC STAL-EDUCATION',
+            '2800258010' => 'SQ-EDUCATION',
+*/        ],
         'budgets' => [
             '746212' => 'COENV MARCOM',
         ],
