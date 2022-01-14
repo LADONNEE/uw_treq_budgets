51b36c2f0112a516d436e9910c7e6feb14f1fe8d
Nadhir <nadhir@ladonnee.com>
2022-01-14 10:42:12 +0100

    Changed Budgets Scope

--- config/budgets.php
+++ config/budgets.php
@@ -6,21 +6,13 @@
 return [
     'scope' => [
         'college-codes' => [
-            '258' => 'COLLEGE OF EDUCATION',
+            '263' => 'COLLEGE OF ENVIRNMNT',
         ],
         'org-codes' => [
             '2552158000' => 'AP DEG - EDUCATION',
-            '2552258000' => 'AP CR CERT-EDUCATION',
-            '2552558000' => 'AP NC STAL-EDUCATION',
-            '2800258010' => 'SQ-EDUCATION',
         ],
         'budgets' => [
-            '098270' => 'ISLANDWOOD-BAINBRIDGE',
-            '098650' => 'NATIVE ED CONTRACT',
-            '098442' => 'PRACTICUM BACB',
-            '098647' => 'NATIVE ED CERT',
-            '098669' => 'PRACTICE BASED COACH',
-            '098703' => 'ISLANDWOOD -UW SEATTLE',
+            '746212' => 'COENV MARCOM',
         ],
     ]
 ];
