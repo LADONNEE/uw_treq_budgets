328e9e5221c7e915b47c36557552491b6a26ca3e
Nadhir <nadhir@ladonnee.com>
2022-10-20 21:39:40 +0200

    Fix static

--- app/Forms/Budget/ManagerForm.php
+++ app/Forms/Budget/ManagerForm.php
@@ -49,7 +49,7 @@ class ManagerForm extends Form
     public static function optionsManagers()
     {
         $out = ['' => '(no budget manager)'];
-        $fiscals = DB::table($this->table_uw_persons . ' AS p')
+        $fiscals = DB::table(Config::get('app.database_shared') . '.uw_persons AS p')
             ->join('managers_view', 'managers_view.person_id', '=', 'p.person_id')
             ->select('managers_view.person_id', 'p.firstname', 'p.lastname')
             ->orderBy('p.firstname')
