cea1abc16ed02fa525d42cf81da7a487aa0f419c
Nadhir <nadhir@ladonnee.com>
2023-08-31 16:33:42 +0200

    Fix Migration

--- database/migrations/2022_10_31_105008_create_worktags_table.php
+++ database/migrations/2022_10_31_105008_create_worktags_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-Class WorktagsTable extends Migration
+Class CreateWorktagsTable extends Migration
 {
     public function up()
     {
--- database/migrations/2022_10_31_105020_create_worktags_budgets_fdm_translator_table.php
+++ database/migrations/2022_10_31_105020_create_worktags_budgets_fdm_translator_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-Class WorktagsBudgetsFdmTranslatorTable extends Migration
+Class CreateWorktagsBudgetsFdmTranslatorTable extends Migration
 {
     public function up()
     {
